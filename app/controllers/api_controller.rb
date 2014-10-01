82ec2084c05f0103e44a7b1ae568a914851f53c3
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2014-10-01 19:17:06 +0000

    manual cors

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,17 @@
 class ApiController < ApplicationController
   respond_to :json
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :handle_cors
   
   private
   def record_not_found
     head :not_found
+  end
+
+  def handle_cors
+    headers['Access-Control-Allow-Origin']   = '*'
+    headers['Access-Control-Allow-Methods']  = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers']  = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
